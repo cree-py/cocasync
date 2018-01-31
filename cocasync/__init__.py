c885e87788ae84c1f88ea7134600617be9388699
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2018-01-31 15:55:53 -0600

    change version?

--- cocasync/__init__.py
+++ cocasync/__init__.py
@@ -3,5 +3,5 @@ from .client import *
 __author__ = 'Umbresp'
 __title__ = 'cocasync'
 __license__ = 'MIT'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __github__ = 'https://www.github.com/cree-py/cocasync'
--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup, find_packages
-with open('version.txt', 'r') as f:
-    version = f.read()
+
 setup(
     name='cocasync',
-    version=version,
+    version='0.0.2',
     description='An async Python API wrapper for the COC API',
     long_description="async python wrapper for the COC API. Also, we don't yet have a good description so this empty page will have to do.",
     url='https://github.com/cree-py/cocasync',
