fb87d710cad432e9d304257509f82517d1111dc5
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2018-01-31 15:33:10 -0600

    fuck it

--- cocasync/__init__.py
+++ cocasync/__init__.py
@@ -1,15 +1,7 @@
 from .client import *
-import os
-
-script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-rel_path = "version.txt"
-abs_file_path = os.path.join(script_dir, rel_path)
-
-with open(abs_file_path, "r") as f:
-    version = f.read()
 
 __author__ = 'Umbresp'
 __title__ = 'cocasync'
 __license__ = 'MIT'
-__version__ = version
+__version__ = '0.0.1'
 __github__ = 'https://www.github.com/cree-py/cocasync'
