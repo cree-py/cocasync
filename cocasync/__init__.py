3881f7a7106a945d256944e2dedc5994e28d461c
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2018-01-31 15:29:47 -0600

    iopps

--- cocasync/__init__.py
+++ cocasync/__init__.py
@@ -1,6 +1,10 @@
 from .client import *
 
-with open("../version.txt", "r") as f:
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+rel_path = "../version.txt"
+abs_file_path = os.path.join(script_dir, rel_path)
+
+with open(abs_file_path, "r") as f:
     version = f.read()
 
 __author__ = 'Umbresp'
